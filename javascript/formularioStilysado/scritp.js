bb0e1d10d7c4b0e96428fa6a64069d7e36434d30
CaiqueFernandes <kakaifernandes952@gmail.com>
2025-11-25 22:02:55 -0300

    .

--- javascript/formularioStilysado/scritp.js
+++ javascript/formularioStilysado/scritp.js
@@ -1,23 +1,30 @@
 function ativar() {
-  const nome = document.getElementById("nome");
-  const email = document.getElementById("email");
-  const cpf = document.getElementById("cpf");
-  const primeiraSenha = document.getElementById("senha");
-  const segundaSenha = document.getElementById("comfirmarSenha");
-  const mensagem = document.getElementById("caixaMensagem");
+    const nome = document.getElementById("nome");
+    const email = document.getElementById("email");
+    const cpf = document.getElementById("cpf");
+    const primeiraSenha = document.getElementById("senha");
+    const segundaSenha = document.getElementById("comfirmarSenha");
+    const mensagem = document.getElementById("caixaMensagem");
+    const form = document.getElementById("formActive");
 
-  if (primeiraSenha.value === segundaSenha.value) {
-    alert("O formulário foi enviado com sucesso");
-    return true;
-  } else {
-    alert("Os campos de senha não batem tente novamente");
-  }
-  return false;
+    if (primeiraSenha.value === segundaSenha.value) {
+        mensagem.innerHTML = "<h1>O formulário foi enviado com sucesso</h1>";
+        resetForm()
+        return true;
+    } else {
+        alert("Os campos de senha não batem tente novamente");
+    }
+    console.log(mensagem)
+
+    return false;
+
 }
 
-function formActive() {
-  ativar();
+function formActive(event) {
+    event.preventDefault();
+
+    ativar();
 }
 function resetForm() {
-  document.getElementById("formActive").reset();
+    form.reset();
 }
